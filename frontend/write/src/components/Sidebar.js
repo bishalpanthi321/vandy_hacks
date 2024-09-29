62daa023261fa2eb9c8d11bbccc55757964fc979
Gnik Droy <gnikdroy@gmail.com>
2024-09-28 23:15:43 -0500

    merge code

--- frontend/write/src/components/Sidebar.js
+++ frontend/write/src/components/Sidebar.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { faUser } from '@fortawesome/free-solid-svg-icons'; // Example icon
 import { faSignIn } from '@fortawesome/free-solid-svg-icons'; // Example icon
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import FileList from './FileList';
+import Login from './Login';
 
 export default function Sidebar() {
+  const [showLoginModal, setShowLoginModal] = useState(false);
+  const handleLoginModalOpen = () => setShowLoginModal(true);
+  const handleLoginModalClose = () => setShowLoginModal(false);
+
+
   return <div className="d-flex flex-column justify-content-between align-items-center h-100 p-2 bg-dark text-white" style={{ minWidth: "40ch" }}>
     <div>
       <h2 class="mb-5"> Perplexity </h2>
@@ -16,11 +22,12 @@ export default function Sidebar() {
         &nbsp;
         Sign Up
       </button>
-      <button class="btn btn-outlined btn-primary" onClick={onLoginClick}>
+      <button class="btn btn-outlined btn-primary" onClick={handleLoginModalOpen}>
         <FontAwesomeIcon icon={faSignIn} />
         &nbsp;
         Login
       </button>
     </div>
+    <Login onShow={showLoginModal} onHideClick={handleLoginModalClose} />
   </div>;
 }
--- frontend/write/src/pages/Home.js
+++ frontend/write/src/pages/Home.js
@@ -1,18 +1,11 @@
 import Sidebar from "../components/Sidebar";
 import Editor from "../components/Editor";
-import Login from "../components/login";
-import {useState} from 'react';
 
 export default function Home() {
-    const [showLoginModal, setShowLoginModal] = useState(false);
-    const handleLoginModalOpen = () => setShowLoginModal(true);
-    const handleLoginModalClose = () => setShowLoginModal(false);
-    
     return (
         <div class="vh-100 vw-100 d-flex flex-row">
-            <Sidebar onLoginClick={handleLoginModalOpen}/>
+            <Sidebar />
             <Editor />
-            <Login onShow= {showLoginModal} onHideClick={handleLoginModalClose}/>
         </div>
     );
 }
