596a5b36c38a66e9fbe42b9429b0355da1cf3004
10varun17 <17varun.rayamajhi@gmail.com>
2024-09-28 23:03:20 -0500

    login added

--- frontend/write/src/components/Sidebar.js
+++ frontend/write/src/components/Sidebar.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { faUser } from '@fortawesome/free-solid-svg-icons'; // Example icon
 import { faSignIn } from '@fortawesome/free-solid-svg-icons'; // Example icon
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import FileList from './FileList';
 
-export default function Sidebar({onLoginClick}) {
+export default function Sidebar({ onLoginClick }) {
   const [collapsed, setCollapsed] = useState(false);
   return <div className="d-flex flex-column justify-content-between align-items-center h-100 p-2 bg-dark text-white" style={{ minWidth: "35ch" }}>
     <div>
--- frontend/write/src/pages/Home.js
+++ frontend/write/src/pages/Home.js
@@ -1,11 +1,18 @@
 import Sidebar from "../components/Sidebar";
 import Editor from "../components/Editor";
+import Login from "../components/login";
+import {useState} from 'react';
 
 export default function Home() {
+    const [showLoginModal, setShowLoginModal] = useState(false);
+    const handleLoginModalOpen = () => setShowLoginModal(true);
+    const handleLoginModalClose = () => setShowLoginModal(false);
+    
     return (
         <div class="vh-100 vw-100 d-flex flex-row">
-            <Sidebar />
+            <Sidebar onLoginClick={handleLoginModalOpen}/>
             <Editor />
+            <Login onShow= {showLoginModal} onHideClick={handleLoginModalClose}/>
         </div>
     );
 }
